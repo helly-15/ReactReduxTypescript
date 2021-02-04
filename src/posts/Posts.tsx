b4b8887523d00260003f1a5b01e6f828caf184f9
Lenocka <lvutina.en@gmail.com>
2021-02-04 15:27:37 +0300

    feat:add subscriptions

--- src/posts/Posts.tsx
+++ src/posts/Posts.tsx
@@ -1,27 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import {data} from '../data/data';
 import {IPostsInterface} from './postsInterface';
 export const Posts:React.FC<IPostsInterface> = (props)=> {
     const {signedUser} = props;
+    const [showAllPosts, setShowAllPosts] = useState<boolean>(true);
+
 
     const postElements = data.posts.byId.map((post)=>{
-        return <div className='posts-post'>
-            <h3> {post.author}</h3>
-            <h4> {post.body}</h4>
-        </div>
+        let userSubscriptions = data.users.byId.filter(user=>user.name === signedUser)[0].subscriptions;
+        if (showAllPosts){
+            return <div className='posts-post card'>
+                <h3 className='card-title text-left'> {post.author}</h3>
+                <h4 className='card-text text-left'> {post.body}</h4>
+            </div>
+        }
+        else if (userSubscriptions.includes(post.author)){
+            return <div className='posts-post card'>
+                <h3 className='card-title text-left'> {post.author}</h3>
+                <h4 className='card-text text-left'> {post.body}</h4>
+            </div>
+        }
+
     })
 
     return (
-        <div className='posts'>
-            <p> Hello {signedUser}!</p>
-            <button>
+        <div className='posts card-body text-center col-sm-3'>
+            <p className="card-text card-header"> Hello {signedUser}!</p>
+            <button className="btn btn-outline-primary" onClick={()=>setShowAllPosts(true)}>
                 All posts
             </button>
-            <button>
+            <button className="btn btn-outline-primary" onClick={()=>setShowAllPosts(false)}>
                 Subscriptions
             </button>
             {postElements}
-            <button>
+            <button className="btn btn-outline-info">
                 To profile
             </button>
         </div>
