0658edb87c9b8bfb4efb26a3d42f4092ffa66965
Lenocka <lvutina.en@gmail.com>
2021-02-18 11:07:53 +0300

    feat:add styles to signup

--- src/components/signup/Signup.tsx
+++ src/components/signup/Signup.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import {ISignupInterface} from "./ISignupInterface";
-import './Signup.css'
+import './Signup.scss'
 
 export const Signup: React.FC<ISignupInterface> = (props) => {
     const {onSignUp, onUserEnter, signedUser} = props;
     return (
         <form className='signup'>
-            <p>
+            <p className='signup-welcome'>
                 Welcome!
             </p>
-            <input type='text' maxLength={12} onChange={onUserEnter}/>
-            <button type='submit' onClick={onSignUp} disabled={signedUser.length <= 3}>Sign up</button>
+            <input className='signup-text' type='text' maxLength={12} onChange={onUserEnter} placeholder={"What's your name?"}/>
+            <button className='signup-submit' type='submit' onClick={onSignUp} disabled={signedUser.length <= 3}>Sign up</button>
         </form>
     );
 }
